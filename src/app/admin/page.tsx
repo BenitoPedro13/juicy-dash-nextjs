086cab3ddd5efaedfac90394cc793ec090648c75
Benito Pedro <benitopedro21@outlook.com>
2023-07-12 19:06:46 -0300

    wip

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,16 +1,31 @@
 "use client"
 import * as React from "react";
-import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
+import { useEffect, useState } from "react";
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
-const AdminApp = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={ListGuesser} edit={EditGuesser} recordRepresentation="name" />
-    <Resource name="posts" list={ListGuesser} edit={EditGuesser} recordRepresentation="title" />
-    <Resource name="comments" list={ListGuesser} edit={EditGuesser} />
-  </Admin>
-);
+const AdminApp = () => {
+  const [Admin, setAdmin] = useState<React.JSX.Element | undefined>(undefined);
+
+  useEffect(() => {
+    const loadAdmin = async () => {
+      const { Admin, Resource, ListGuesser, EditGuesser } = await import('react-admin');
+      setAdmin(() => (
+        <Admin dataProvider={dataProvider}>
+          <Resource name="users" list={ListGuesser} edit={EditGuesser} recordRepresentation="name" />
+          <Resource name="posts" list={ListGuesser} edit={EditGuesser} recordRepresentation="title" />
+          <Resource name="comments" list={ListGuesser} edit={EditGuesser} />
+        </Admin>
+      ));
+    };
+
+    if (typeof document !== 'undefined') {
+      loadAdmin();
+    }
+  }, []);
+
+  return <>{Admin}</>;
+};
 
 export default AdminApp;
