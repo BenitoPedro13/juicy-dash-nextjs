eae30f394353e62eaa7b9f3ccf278130ef25361a
Benito Pedro <benitopedro21@outlook.com>
2023-07-15 14:34:01 -0300

    wip side nav mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import Metrics from "@/components/Metrics";
 import { Inter } from "next/font/google";
 import CostPerMetric from "@/components/CostPerMetric";
 import MetricsLineGraph from "@/components/MetricsLineGraph";
+import SidenavDesktop from "@/components/SidenavDesktop";
 import MetricsDoughnutGraph from "@/components/MetricsDoughnutGraph";
 import CreatorsTable from "@/components/CreatorsTable/CreatorsTable";
 import useDataStore, { Influencer } from "@/store";
@@ -53,50 +54,7 @@ export default function Home() {
 
   return (
     <main>
-      <div className="hidden w-[82px] h-[100vh] xl:flex flex-row justify-start items-start bg-white overflow-hidden z-1 p-0 content-start flex-nowrap gap-0 fixed rounded-none border-[#475466] border-solid border-r z-10">
-        <div className="w-[81px] h-[100vh] flex flex-col justify-between items-start overflow-visible flex-grow flex-shrink-0 relative p-0 content-start flex-nowrap rounded-none">
-          <nav className="navbar box-border flex-shrink-0 w-100 h-min flex flex-col justify-start items-center pt-8 overflow-visible relative content-center flex-nowrap gap-6 rounded-none">
-            <Link href="/">
-              <Image src={logo} width={42} height={38} alt="Juicy Logo" />
-            </Link>
-            <div className="box-border flex-shrink-0 w-full h-[48px] flex justify-center items-start px-4 overflow-visible relative content-start flex-nowrap gap-2 rounded-none">
-              <button className="btn btn-circle btn-outline box-border flex-shrink-0 w-12 h-12 flex flex-row justify-start items-start p-3 bg-[#ff77ef] overflow-hidden relative content-start flex-nowrap gap-2 rounded-full">
-                <svg
-                  width="20"
-                  height="21"
-                  viewBox="-1 -1 20 21"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M0 5.41255C0 3.73239 0 2.89231 0.32698 2.25058C0.614601 1.68609 1.07354 1.22715 1.63803 0.939529C2.27976 0.612549 3.11984 0.612549 4.8 0.612549H13.2C14.8802 0.612549 15.7202 0.612549 16.362 0.939529C16.9265 1.22715 17.3854 1.68609 17.673 2.25058C18 2.89231 18 3.73239 18 5.41255V13.8125C18 15.4927 18 16.3328 17.673 16.9745C17.3854 17.539 16.9265 17.9979 16.362 18.2856C15.7202 18.6125 14.8802 18.6125 13.2 18.6125H4.8C3.11984 18.6125 2.27976 18.6125 1.63803 18.2856C1.07354 17.9979 0.614601 17.539 0.32698 16.9745C0 16.3328 0 15.4927 0 13.8125V5.41255Z"
-                    fill="white"
-                  ></path>
-                  <path d="M5 12.6125V14.6125V12.6125Z" fill="white"></path>
-                  <path d="M9 8.61255V14.6125V8.61255Z" fill="white"></path>
-                  <path d="M13 4.61255V14.6125V4.61255Z" fill="white"></path>
-                  <path
-                    d="M5 12.6125V14.6125M9 8.61255V14.6125M13 4.61255V14.6125M4.8 18.6125H13.2C14.8802 18.6125 15.7202 18.6125 16.362 18.2856C16.9265 17.9979 17.3854 17.539 17.673 16.9745C18 16.3328 18 15.4927 18 13.8125V5.41255C18 3.73239 18 2.89231 17.673 2.25058C17.3854 1.68609 16.9265 1.22715 16.362 0.939529C15.7202 0.612549 14.8802 0.612549 13.2 0.612549H4.8C3.11984 0.612549 2.27976 0.612549 1.63803 0.939529C1.07354 1.22715 0.614601 1.68609 0.32698 2.25058C0 2.89231 0 3.73239 0 5.41255V13.8125C0 15.4927 0 16.3328 0.32698 16.9745C0.614601 17.539 1.07354 17.9979 1.63803 18.2856C2.27976 18.6125 3.11984 18.6125 4.8 18.6125Z"
-                    stroke="#0A0A0A"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  ></path>
-                </svg>
-              </button>
-            </div>
-          </nav>
-          <div className="w-full h-[72px] flex flex-col justify-start items-start pb-6 px-4 overflow-visible relative content-start flex-nowrap gap-6 rounded-none">
-            <Image
-              src={buserLogo}
-              width={48}
-              height={48}
-              alt="Buser Logo"
-              className="btn btn-circle btn-outline border-black border-[1px] border-solid"
-            />
-          </div>
-        </div>
-      </div>
+      <SidenavDesktop />
       <div className="w-full h-min flex flex-row justify-start items-start bg-white overflow-hidden p-0 xl:pl-[82px] content-start flex-nowrap gap-0 rounded-none absolute">
         <div className="box-border flex-shrink-0 xl:w-[calc(100%-379px)] w-full h-min flex flex-col justify-start items-center pt-8 pb-12 overflow-visible content-center flex-nowrap gap-[28px] rounded-none">
           <div className="box-border flex-shrink-0 w-full h-auto flex flex-col justify-center items-start px-8 overflow-visible relative content-start flex-nowrap gap-3 rounded-none">
@@ -169,7 +127,7 @@ export default function Home() {
             </div>
           </div>
           <div className="w-full flex-shrink-0 h-min flex flex-col justify-start items-start overflow-visible relative xl:px-[22px] p-0 content-start flex-nowrap gap-6 rounded-none">
-            <div className="box-border flex-shrink-0 w-full h-min flex flex-col justify-start items-start px-[15px] overflow-visible relative content-start flex-nowrap xl:gap-[22px] gap-6 rounded-none">
+            <div className="box-border flex-shrink-0 w-full h-min flex flex-col justify-start items-start xl:p-0 px-[15px] overflow-visible relative content-start flex-nowrap xl:gap-[22px] gap-6 rounded-none">
               <div className="flex-shrink-0 flex-grow xl:flex-grow-0 w-full h-min flex xl:flex-row flex-col justify-start items-center overflow-visible relative p-0 content-center flex-nowrap xl:gap-5 gap-[15px] rounded-none">
                 <CostPerMetric
                   sigla="CPE"
---
+++ src/components/SidenavDesktop.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import logo from "@/../public/favicon.png";
+import buserLogo from "@/../public/buser-logo.webp";
+import Link from "next/link";
+import Image from "next/image";
+
+const SidenavDesktop = () => {
+  return (
+    <div className="hidden w-[82px] h-[100vh] xl:flex flex-row justify-start items-start bg-white overflow-hidden z-1 p-0 content-start flex-nowrap gap-0 fixed rounded-none border-[#475466] border-solid border-r z-10">
+    <div className="w-[81px] h-[100vh] flex flex-col justify-between items-start overflow-visible flex-grow flex-shrink-0 relative p-0 content-start flex-nowrap rounded-none">
+      <nav className="navbar box-border flex-shrink-0 w-100 h-min flex flex-col justify-start items-center pt-8 overflow-visible relative content-center flex-nowrap gap-6 rounded-none">
+        <Link href="/">
+          <Image src={logo} width={42} height={38} alt="Juicy Logo" />
+        </Link>
+        <div className="box-border flex-shrink-0 w-full h-[48px] flex justify-center items-start px-4 overflow-visible relative content-start flex-nowrap gap-2 rounded-none">
+          <button className="btn btn-circle btn-outline box-border flex-shrink-0 w-12 h-12 flex flex-row justify-start items-start p-3 bg-[#ff77ef] overflow-hidden relative content-start flex-nowrap gap-2 rounded-full">
+            <svg
+              width="20"
+              height="21"
+              viewBox="-1 -1 20 21"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0 5.41255C0 3.73239 0 2.89231 0.32698 2.25058C0.614601 1.68609 1.07354 1.22715 1.63803 0.939529C2.27976 0.612549 3.11984 0.612549 4.8 0.612549H13.2C14.8802 0.612549 15.7202 0.612549 16.362 0.939529C16.9265 1.22715 17.3854 1.68609 17.673 2.25058C18 2.89231 18 3.73239 18 5.41255V13.8125C18 15.4927 18 16.3328 17.673 16.9745C17.3854 17.539 16.9265 17.9979 16.362 18.2856C15.7202 18.6125 14.8802 18.6125 13.2 18.6125H4.8C3.11984 18.6125 2.27976 18.6125 1.63803 18.2856C1.07354 17.9979 0.614601 17.539 0.32698 16.9745C0 16.3328 0 15.4927 0 13.8125V5.41255Z"
+                fill="white"
+              ></path>
+              <path d="M5 12.6125V14.6125V12.6125Z" fill="white"></path>
+              <path d="M9 8.61255V14.6125V8.61255Z" fill="white"></path>
+              <path d="M13 4.61255V14.6125V4.61255Z" fill="white"></path>
+              <path
+                d="M5 12.6125V14.6125M9 8.61255V14.6125M13 4.61255V14.6125M4.8 18.6125H13.2C14.8802 18.6125 15.7202 18.6125 16.362 18.2856C16.9265 17.9979 17.3854 17.539 17.673 16.9745C18 16.3328 18 15.4927 18 13.8125V5.41255C18 3.73239 18 2.89231 17.673 2.25058C17.3854 1.68609 16.9265 1.22715 16.362 0.939529C15.7202 0.612549 14.8802 0.612549 13.2 0.612549H4.8C3.11984 0.612549 2.27976 0.612549 1.63803 0.939529C1.07354 1.22715 0.614601 1.68609 0.32698 2.25058C0 2.89231 0 3.73239 0 5.41255V13.8125C0 15.4927 0 16.3328 0.32698 16.9745C0.614601 17.539 1.07354 17.9979 1.63803 18.2856C2.27976 18.6125 3.11984 18.6125 4.8 18.6125Z"
+                stroke="#0A0A0A"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </nav>
+      <div className="w-full h-[72px] flex flex-col justify-start items-start pb-6 px-4 overflow-visible relative content-start flex-nowrap gap-6 rounded-none">
+        <Image
+          src={buserLogo}
+          width={48}
+          height={48}
+          alt="Buser Logo"
+          className="btn btn-circle btn-outline border-black border-[1px] border-solid"
+        />
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default SidenavDesktop
