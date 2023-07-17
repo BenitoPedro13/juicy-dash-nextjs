0ce29a9288e6d843cc8125f2fa5cf5771c4c8991
Benito Pedro <benitopedro21@outlook.com>
2023-07-17 16:56:52 -0300

    upload button

--- src/components/FileUploadButton.tsx
+++ src/components/FileUploadButton.tsx
@@ -1,4 +1,5 @@
 import useDataStore from "@/store";
+import { motion } from "framer-motion";
 import React, { useRef } from "react";
 
 const FileUploadButton = () => {
@@ -27,7 +28,7 @@ const FileUploadButton = () => {
       .then(async (response) => {
         const res = await response.json();
         console.log("upload response:", res);
-        await fetchAttachment()
+        await fetchAttachment();
       })
       .catch((error) => {
         console.log("upload error:", error);
@@ -36,7 +37,25 @@ const FileUploadButton = () => {
 
   return (
     <div>
-      <button onClick={handleButtonClick}>Upload File</button>
+      <div
+        onClick={handleButtonClick}
+        className="btn btn-ghost box-border flex-shrink-0 w-min h-auto flex justify-center items-center py-[10px] px-[8px] shadow-cost-per-metrics bg-white overflow-hidden self-stretch relative content-center flex-nowrap gap-2 rounded-lg border border-solid border-[#cfd4dc]"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.25}
+          stroke="#2d3442"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+          />
+        </svg>
+      </div>
       <input
         type="file"
         ref={fileInputRef}
