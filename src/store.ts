f68192cc0ad527a4081f4c09f9c8556d369bde5a
Benito Pedro <benitopedro21@outlook.com>
2023-07-20 09:46:11 -0300

    baseApiUrl for DEV

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,8 @@
 import { create } from "zustand";
 import { setCookie } from "nookies";
 
-export const baseApiUrl = 'https://benitopedro.tech'
+// export const baseApiUrl = 'https://benitopedro.tech'
+export const baseApiUrl = 'http://localhost:3000'
 
 export interface Influencer {
   id: number;
