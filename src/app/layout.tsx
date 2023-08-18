163f96e524c6fbb45a779b224d8701f21fe4eb10
Benito Pedro <benitopedro21@outlook.com>
2023-08-17 23:16:33 -0300

    Alterado favicon para letra J

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <link rel="shortcut icon" href="/favicon.ico" />
-      </Head>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
