d04deb020700be92aa96d4e471a3c98af5004cee
Benito Pedro <benitopedro21@outlook.com>
2023-08-17 23:08:23 -0300

    Alterado favicon para letra `J`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,29 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { Plus_Jakarta_Sans } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ['latin'] })
-const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Juicy Dash',
-  description: 'Monitore todos os seus projetos criativos em uma experiência mágica e suculenta',
-}
+  title: "Juicy Dash",
+  description:
+    "Monitore todos os seus projetos criativos em uma experiência mágica e suculenta",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-        
-      </body>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
